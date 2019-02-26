6dd9fa54c735b4ea6f9a03a58bf351ac762103e2
jianxinliu <1159322990@qq.com>
2019-02-26 04:00:56 -0800

    Scanner 测试并修改完成

--- src/main/java/com/minister/pm/core/Scanner.java
+++ src/main/java/com/minister/pm/core/Scanner.java
@@ -2,12 +2,14 @@ package com.minister.pm.core;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.minister.pm.define.App;
+import com.minister.pm.define.Autowired;
 import com.minister.pm.define.Component;
 import com.minister.pm.define.RestController;
 import com.minister.pm.define.URLMapping;
@@ -21,13 +23,24 @@ import com.minister.pm.define.URLMapping;
  */
 public class Scanner {
 
-	private static Context ctx = new Context();
+	private static Context ctx;
 
+	// 扫描的思路需要变，实际应用时，需要扫描的代码和这个框架的代码不在一起，只需要扫描实际代码（实际业务代码）即可，而不是框架代码
+	// 应该以 @App 注解所在的类的位置开始扫描全包
 	private static String pwd = System.getProperty("user.dir") + "/src/main/java";/// home/ljx/data_code/sts_workspace/pm
 
-	public static void main(String[] args) throws IOException {
-//		convertToJavaPath(Paths.get("/home/ljx/data_code/sts_workspace/pm/src/main/java/com/minister/pm/util/PMConfig.java"));
+//	public static void main(String[] args) throws IOException {
+////		convertToJavaPath(Paths.get("/home/ljx/data_code/sts_workspace/pm/src/main/java/com/minister/pm/util/PMConfig.java"));
+//		rootTrval(pwd);
+//	}
+	
+	public Scanner(Context ctx) {
+		this.ctx = ctx;
+	}
+	
+	public Context run() {
 		rootTrval(pwd);
+		return ctx;
 	}
 
 	/**
@@ -45,12 +58,11 @@ public class Scanner {
 						String javaPath = convertToJavaPath(path);
 						try {
 							Class<?> clz = Class.forName(javaPath);
-//							System.out.println(clz.getName());
+							System.out.println(clz.getName());
 							findClassAnnotation(clz);
 						} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 							e.printStackTrace();
 						}
-						System.out.println(path);
 					}
 				}
 			});
@@ -89,29 +101,39 @@ public class Scanner {
 	 * @throws IllegalAccessException
 	 */
 	private static void findClassAnnotation(Class<?> clz) throws InstantiationException, IllegalAccessException {
+		letComponentWired(clz);
+		
 		Annotation[] clzAnnos = clz.getAnnotations();
+		URLMapping classReqMap = clz.getAnnotation(URLMapping.class);
+		String reqMethod = "";
+		String pUrl = "";
+		// 如果类体定义了 URLMapping 则次类内的所有方法接受的请求类型均为此注解指定的方法
+		if (classReqMap != null) {
+			// 方法内不再查看 URLMapping 的请求方法
+			reqMethod = classReqMap.method();
+			pUrl = classReqMap.value();
+		}
+		int i = 1;
+		// 当前类体上是否由 URLMapping 注解
 		for (Annotation an : clzAnnos) {
+			System.out.println((i++) +"   "+ an.toString());
 			Class<? extends Annotation> annoType = an.annotationType();
-			if (annoType.isAnnotationPresent(Component.class)) {
-				ctx.components.put(clz.getSimpleName(), clz.newInstance());
-			} else if (annoType.isAnnotationPresent(RestController.class)) {
-				// 当前类体上是否由 URLMapping 注解
-				boolean hasPUrl = clz.isAnnotationPresent(URLMapping.class);
+			if (clz.isAnnotationPresent(Component.class)) {
+				ctx.components.put(clz.getSimpleName(), clz);
+			} else if (clz.isAnnotationPresent(RestController.class)) {
 				Method[] methods = clz.getMethods();
-				String reqMethod = "";
-				if (hasPUrl) {// 如果类体定义了 URLMapping 则次类内的所有方法接受的请求类型均为此注解指定的方法
-					reqMethod = ((URLMapping) an).method();// 方法内不再查看 URLMapping 的请求方法
-				}
+				
 				for (Method m : methods) {
 					if (m.isAnnotationPresent(URLMapping.class)) {
 						URLMapping mtdAnno = m.getAnnotation(URLMapping.class);
 						String url = "";
 						// 下级路由，定义在方法上
 						String sUrl = mtdAnno.value();
-						if (hasPUrl) {// 如果有上级路由，则将下级路由拼接在上级路由之后
-							String pUrl = ((URLMapping) an).value();
+						// 如果有上级路由，则将下级路由拼接在上级路由之后
+						if (classReqMap != null) {
 							url = pUrl + sUrl;
-						} else {// 没有定义上级路由，则方法的路由作为最终路由，方法的请求方法作为最终请求方法
+						} else {
+							// 没有定义上级路由，则方法的路由作为最终路由，方法的请求方法作为最终请求方法
 							reqMethod = mtdAnno.method();
 							url = sUrl;
 						}
@@ -122,8 +144,32 @@ public class Scanner {
 						ctx.mappers.put(url, handlerName + ":" + reqMethod);
 					}
 				}
-			} else if (annoType.isAnnotationPresent(App.class)) {
+			} else if (clz.isAnnotationPresent(App.class)) {
 
+			} 
+		}
+	}
+	
+	/**
+	 * 自动植入被依赖的组件
+	 * TODO: 从 Components 列表中查找，若没有则报异常
+	 * @param clz
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public static void letComponentWired(Class clz) throws InstantiationException, IllegalAccessException {
+		Object newInstance = clz.newInstance();
+		// auto wire
+//		if(clz.isAnnotationPresent(Component.class)) {
+		Field[] fields = clz.getDeclaredFields();
+		for (int j = 0; j < fields.length; j++) {
+			Field f = fields[j];
+			if (f.isAnnotationPresent(Autowired.class)) {
+				Class<?> type = f.getType();
+				f.setAccessible(true);
+				if (f.get(newInstance) == null) {
+					f.set(newInstance, type.newInstance());
+				}
 			}
 		}
 	}
